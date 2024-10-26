ce078b9e4d97c5734b4a2c4b1757c0d945f8daad
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2024-10-27 00:20:59 +0530

    Time: 8 ms (96.72%), Space: 126.4 MB (92.60%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,28 +2,48 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         
-        vector<int> positiveIntegerList;
-        vector<int> negativeIntegerList;
+//         Approach 1:
+        
+//         vector<int> positiveIntegerList;
+//         vector<int> negativeIntegerList;
 
-        vector<int> finalResult;
+//         vector<int> finalResult;
 
-        for(int i=0; i<nums.size(); i++) {
+//         for(int i=0; i<nums.size(); i++) {
+//             if(nums[i] > 0) {
+//                 positiveIntegerList.push_back(nums[i]);
+//             }
+
+//             else if(nums[i] < 0) {
+//                 negativeIntegerList.push_back(nums[i]);
+//             }
+
+//         }
+
+//         for(int i=0; i<nums.size()/2; i++) {
+//             finalResult.push_back(positiveIntegerList[i]);
+//             finalResult.push_back(negativeIntegerList[i]);
+//         }
+
+//         return finalResult;
+        
+//         Approach 2:
+        vector<int> result(nums.size(), 0);
+        int posIndex = 0;
+        int negIndex = 1;
+        
+        for (int i=0; i<nums.size(); i++) {
             if(nums[i] > 0) {
-                positiveIntegerList.push_back(nums[i]);
+                result[posIndex] = nums[i];
+                posIndex += 2;
             }
-
-            else if(nums[i] < 0) {
-                negativeIntegerList.push_back(nums[i]);
+            else {
+                result[negIndex] = nums[i];
+                negIndex += 2;
             }
-
         }
-
-        for(int i=0; i<nums.size()/2; i++) {
-            finalResult.push_back(positiveIntegerList[i]);
-            finalResult.push_back(negativeIntegerList[i]);
-        }
-
-        return finalResult;
+        
+        return result;
 
     }
 };
