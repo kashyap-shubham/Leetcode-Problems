5a0e68b92b9c7a855c7f6b8b6c09343015ed3492
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2025-02-04 03:16:28 +0530

    Time: 5 ms (77.01%), Space: 12.4 MB (18.3%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -11,26 +11,26 @@ public:
     void deleteNode(ListNode* node) {
 
         // Naive Apporach
-        struct ListNode* p = node;
+        // struct ListNode* p = node;
 
-        while(p->next->next != NULL) {
-            p->val = p->next->val;
-            p = p->next;
-        }
-        p->val = p->next->val;
-        struct ListNode *temp = p->next;
-        p->next = NULL;
-        delete temp;
+        // while(p->next->next != NULL) {
+        //     p->val = p->next->val;
+        //     p = p->next;
+        // }
+        // p->val = p->next->val;
+        // struct ListNode *temp = p->next;
+        // p->next = NULL;
+        // delete temp;
 
 
         // Better Approach
-        // struct ListNode *temp;
-        // temp = node->next;
+        struct ListNode *temp;
+        temp = node->next;
 
-        // node->val = node->next->val;
-        // node->next = node->next->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
         
-        // delete temp;
+        delete temp;
 
     }
 };
