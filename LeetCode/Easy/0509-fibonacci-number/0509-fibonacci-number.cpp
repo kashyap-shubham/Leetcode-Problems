20560353c6a227ad734c2aad4a564677e3a54d49
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-02-27 21:06:58 +0530

    0 ms (100%) 7.7 MB (87.28%) 02-27-2026 0509-fibonacci-number Easy .cpp

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) {
-            return 0;
+
+        // first principle approach
+        // if (n == 0 || n == 1) {
+        //     return n;
+        // }
+
+        // return fib(n-1) + fib(n-2);
+
+
+        // better approach
+
+        int a = 0, b = 1;
+        if (n <= 1) return n;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
 
-        else if (n == 1) {
-            return 1;
-        }
+        return b;
 
-        else {
-            return fib(n-1) + fib(n-2);
-        }
     }
 };
