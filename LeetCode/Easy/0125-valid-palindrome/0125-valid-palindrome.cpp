0d257af1fff34db406734dea50debe2247ac639b
Shubham Kashyap <74668746+kashyap-shubham@users.noreply.github.com>
2026-02-28 12:04:39 +0530

    3 ms (30.81%) 9.9 MB (53.86%) 02-28-2026 0125-valid-palindrome Easy .cpp

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,21 +2,45 @@ class Solution {
 public:
     bool isPalindrome(string s) {
 
+
         // first principle approach
-        string cleanString = "";
+        // string cleanString = "";
         
-        
-        for (char c : s) {
-            if (isalnum(c)) {
-                cleanString += tolower(c);
+        // for (char c : s) {
+        //     if (isalnum(c)) {
+        //         cleanString += tolower(c);
+        //     }
+        // }
+
+        // string rev = cleanString;
+        // reverse(rev.begin(), rev.end());
+
+        // return cleanString == rev;
+
+
+        // optimal approach
+        int left = 0, right = s.length()-1;
+
+        while (left < right) {
+
+            if (!isalnum(s[left])) {
+                left++;
+                continue;
             }
-        }
+            
+            if (!isalnum(s[right])) {
+                right--;
+                continue;
+            }
 
-        string rev = cleanString;
-        reverse(rev.begin(), rev.end());
+            if(tolower(s[left]) != tolower(s[right]))
+            return false;
 
-        return cleanString == rev;
+            left++;
+            right--;
 
-        
+        } 
+
+        return true;       
     }
 };
